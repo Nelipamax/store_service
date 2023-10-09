7b89135b1422e8ff50dffd8ff57356e41713e9bb
Egor Filippov <4filippov@gmail.com>
2023-10-09 10:37:22 +0200

    add liquid base

--- src/main/java/ru/aston/store_service/model/Store.java
+++ src/main/java/ru/aston/store_service/model/Store.java
@@ -12,7 +12,7 @@ import lombok.*;
 public class Store {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
